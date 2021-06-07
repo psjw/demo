118c8467bd2b54d05c407ed37432001493c915e5
psjw <45330194+psjw@users.noreply.github.com>
2021-06-07 20:58:53 +0900

    Delete ProductControllerTest.java

--- app/src/test/java/com/codesoom/demo/controllers/ProductControllerTest.java
+++
@@ -1,9 +0,0 @@
-package com.codesoom.demo.controllers;
-
-
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-
-@WebMvcTest(ProductContr)
-class ProductControllerTest {
-
-}
