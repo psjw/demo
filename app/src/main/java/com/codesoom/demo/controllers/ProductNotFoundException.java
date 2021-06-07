704bb43613be8c0c5b35d5596b789b54a078eca3
psjw <45330194+psjw@users.noreply.github.com>
2021-06-07 20:49:53 +0900

    Delete ProductNotFoundException.java

--- app/src/main/java/com/codesoom/demo/controllers/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.codesoom.demo.controllers;
-
-public class TaskNotFoundException extends RuntimeException{
-    public TaskNotFoundException(Long id){
-        super("Task not found : " + id);
-    }
-}
